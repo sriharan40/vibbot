52fcb67df91c852dc0864e4c3939727703a659fa
sriharan40 <sriharan40@gmail.com>
2017-01-20 23:58:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const webhookUrl = process.env.WEBHOOK_URL;
 
 app.use('/webhook', bot.middleware());
 
-app.post('/webhook', function(req, response) {
+app.post('/webhook', bot.middleware(){
 
 console.log("Webhook"+webhookUrl);
 
